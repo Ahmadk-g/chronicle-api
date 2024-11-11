ba53092609148e3d0b15ff9ada3865afd6ee0804
Ahmad Khalaf <khalaf.ahmed@hotmail.com>
2024-11-11 19:32:52 +0000

    Add landing page view and prepare API for deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -30,8 +30,17 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':10,
+    'DATETIME_FORMAT': '%d %b %Y',
 }
+if 'DEV' in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
