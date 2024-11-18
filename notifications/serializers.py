963426d5c63398364cbdac54ee0b104c3c95e5df
Ahmad Khalaf <khalaf.ahmed@hotmail.com>
2024-11-18 16:44:39 +0000

    Add post and event image fields to notifications serializer

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -7,13 +7,15 @@ class NotificationSerializer(serializers.ModelSerializer):
     notifier_name = serializers.ReadOnlyField(source='notifier.username')
     notifier_image = serializers.ReadOnlyField(source='notifier.profile.image.url')
     notifier_id = serializers.ReadOnlyField(source='notifier.profile.id')
+    post_image = serializers.ReadOnlyField(source='post.image.url')
+    event_image = serializers.ReadOnlyField(source='event.image.url')
 
     class Meta:
         model = Notification
         fields = [
             'id', 'owner', 'notifier_name', 'notifier_image',
             'notifier_id','notification_type','post', 'event',
-            'is_read', 'created_at'
+            'post_image', 'event_image', 'is_read', 'created_at'
         ]
 
     def mark_as_read(self, instance):
