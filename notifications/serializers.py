ebd1082c1b7770b56ce7469035f61c3af4e70b9c
Ahmad Khalaf <khalaf.ahmed@hotmail.com>
2024-11-18 16:14:30 +0000

    Add 'notifier_id' to notifications serializer

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -6,13 +6,14 @@ class NotificationSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     notifier_name = serializers.ReadOnlyField(source='notifier.username')
     notifier_image = serializers.ReadOnlyField(source='notifier.profile.image.url')
+    notifier_id = serializers.ReadOnlyField(source='notifier.profile.id')
 
     class Meta:
         model = Notification
         fields = [
             'id', 'owner', 'notifier_name', 'notifier_image',
-            'notification_type','post', 'event', 'is_read',
-            'created_at'
+            'notifier_id','notification_type','post', 'event',
+            'is_read', 'created_at'
         ]
 
     def mark_as_read(self, instance):
